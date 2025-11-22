2b7b73f11b26f18ba34286681fd54418276fd08c
sandromatiass <devsandromatias@gmail.com>
2025-11-22 16:04:37 -0300

    correction deploy vercel

--- build_files.sh
+++ build_files.sh
@@ -6,7 +6,4 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --noinput --clear
 
-# Create necessary directories
-mkdir -p staticfiles
-
 echo "Build completed successfully!"
